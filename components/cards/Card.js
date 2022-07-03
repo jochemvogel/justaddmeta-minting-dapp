94bda76611854fc0e66c48194e25a05cb563815c
Leonhard Kleinfeld <104496345+ogchectoco@users.noreply.github.com>
2022-07-03 18:33:53 +0200

    added style to card, added content to card

--- components/cards/Card.js
+++ components/cards/Card.js
@@ -8,13 +8,31 @@ export default function Card(props) {
             <div class={styles.cardContainer}>
 
             <div class={styles.itemRow}>
-                <div className={styles.cardCol}>1 of 3</div>
+                <div className={styles.cardCol}>
+                <div className={styles.card}>
+                <div className={styles.cardImage}>
+                </div>
+                <div className={styles.cardBody}>
+                <div className={styles.cardTitle}>
+                    <span>Card Title</span>
+                    </div>
+                    <div className={styles.cardText}>
+                    <span>Some quick example text to build on the card title and make up the bulk of
+      the card's content.</span>
+                    </div>
+                    <button className={styles.button}>Enter Phase 1</button>
+                </div>
+                </div>
+                </div>
                 <div className={styles.cardCol}>2 of 3</div>
                 <div className={styles.cardCol}>3 of 3</div>
                 <div className={styles.cardCol}>4 of 4</div>
-            </div>
+            
 
             </div>
+            
+            </div>
+
         </section>
     )
 }
