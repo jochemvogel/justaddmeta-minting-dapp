7784ff5fc847f894fbcf8865e1a204e39ecf29e0
Abbas Tolgay Yilmaz <abbas.tolgay.yilmaz@protonmail.com>
2022-07-04 21:47:59 +0200

    render auth flow window upon Enter Phase 1 click

--- components/AuthCard.js
+++ components/AuthCard.js
@@ -1,7 +1,7 @@
 import React from "react";
 import styles from "../styles/authcard.module.css";
 
-export default function Authcard() {
+export  const AuthCard = ()=> {
   return (
     <section className={styles.sectionCard}>
     <div className={styles.container}>
--- components/Card.js
+++ components/Card.js
@@ -1,85 +1,80 @@
-import React from "react";
+import {React, useState} from "react";
 import styles from "../styles/card.module.css";
+import {AuthCard} from "./AuthCard";
+ 
+export const Card = () => {
+  const [enterPhaseOne, setEnterPhaseOne] = useState(false);
 
-export const Card =() => {
-    return (
-        <section className={styles.cardsPhases}>
-            
-            <div className={styles.cardContainer}>
+  const handleClick = (event) => {
+    // toggle display state
+    // setEnterPhaseOne((current) => !current);
+    // set it to true
+    setEnterPhaseOne(true);
+  };
 
-                <div className={styles.itemRow}>
-                    <div className={styles.cardCol1}>
-                        <div className={styles.card}>
-                            <div className={styles.cardImage}>
-                            </div>
-                            <div className={styles.cardBody}>
-                                <div className={styles.cardLabel}>
-
-                                </div>
-                                <div className={styles.cardText}>
-                                    <h3>Phase 1</h3>
-                                    <label>Minting</label>
-                                </div>
-
-                            </div>
-                        </div>
-                    </div>
-                    <div className={styles.cardCol2}>
-                        <div className={styles.card}>
-                            <div className={styles.cardImage}>
-                            </div>
-                            <div className={styles.cardBody}>
-                                <div className={styles.cardLabel}>
-
-                                </div>
-                                <div className={styles.cardText}>
-                                    <h3>Phase 2</h3>
-                                    <label>Physical Good</label>
-                                </div>
-
-                            </div>
-                        </div>
-                    </div>
-                    <div className={styles.cardCol3}>
-                        <div className={styles.card}>
-                            <div className={styles.cardImage}>
-                            </div>
-                            <div className={styles.cardBody}>
-                                <div className={styles.cardLabel}>
-
-                                </div>
-                                <div className={styles.cardText}>
-                                    <h3>Phase 3</h3>
-                                    <label>Airdrop</label>
-                                </div>
-
-                            </div>
-                        </div>
-                    </div>
-                    <div className={styles.cardCol4}>
-                        <div className={styles.card}>
-                            <div className={styles.cardImage}>
-                            </div>
-                            <div className={styles.cardBody}>
-                                <div className={styles.cardLabel}>
-
-                                </div>
-                                <div className={styles.cardText}>
-                                    <h3>Phase 4</h3>
-                                    <label>Burn</label>
-                                </div>
-
-                            </div>
-                        </div>
-                    </div>
-
-
+  return (
+    <>
+    {enterPhaseOne? (<AuthCard/>): (
+     <section className={styles.cardsPhases}>
+      <div className={styles.cardContainer}>
+        <div className={styles.itemRow}>
+          <div className={styles.cardCol1}>
+            <div className={styles.card}>
+              <div className={styles.cardImage}></div>
+              <div className={styles.cardBody}>
+                <div className={styles.cardLabel}></div>
+                <div className={styles.cardText}>
+                  <h3>Phase 1</h3>
+                  <label>Minting</label>
                 </div>
-                <div className={styles.itemRowButton}>
-                    <button className={styles.button}>Enter Phase 1</button>
-                </div>
+              </div>
             </div>
-
-        </section>
-    )
-}
+          </div>
+          <div className={styles.cardCol2}>
+            <div className={styles.card}>
+              <div className={styles.cardImage}></div>
+              <div className={styles.cardBody}>
+                <div className={styles.cardLabel}></div>
+                <div className={styles.cardText}>
+                  <h3>Phase 2</h3>
+                  <label>Physical Good</label>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className={styles.cardCol3}>
+            <div className={styles.card}>
+              <div className={styles.cardImage}></div>
+              <div className={styles.cardBody}>
+                <div className={styles.cardLabel}></div>
+                <div className={styles.cardText}>
+                  <h3>Phase 3</h3>
+                  <label>Airdrop</label>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className={styles.cardCol4}>
+            <div className={styles.card}>
+              <div className={styles.cardImage}></div>
+              <div className={styles.cardBody}>
+                <div className={styles.cardLabel}></div>
+                <div className={styles.cardText}>
+                  <h3>Phase 4</h3>
+                  <label>Burn</label>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className={styles.itemRowButton}>
+          <button className={styles.button} onClick={handleClick}>
+            Enter Phase 1
+          </button>
+        </div>
+      </div>
+    </section>
+    )}
+    </>
+  );
+};
