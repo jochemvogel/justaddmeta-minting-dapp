6ba43e2f582e6b219c8da4c74d75c77a8fd451a0
Leonhard Kleinfeld <104496345+ogchectoco@users.noreply.github.com>
2022-07-05 14:49:00 +0200

    adjust footer, adjust card, added icons

--- components/Footer.js
+++ components/Footer.js
@@ -1,54 +1,60 @@
 import React from "react";
-
+import emailOutline from "./icons/emailOutline.svg";
 import JustaddmetaLogo from "./JustaddmetaLogo";
 
 import styles from "../styles/footer.module.css";
 export const Footer = () => {
   return (
     <footer className={styles.footer}>
-      <div className={styles.container}>
-        <div className={styles.wrapper}>
-        <div className={styles.colLeft}>
-          <JustaddmetaLogo />
-
-          <p>
-            We enable your brand to <br></br>
-            make a sustainable contribution <br></br>
-            to the metaverse.
-          </p>
-          <button className={styles.button}>Enter Safe House</button>
-          <div className={styles.socialWrapper}>
-            <i class="fa-brands fa-instagram"></i>
-            <i class="fa-brands fa-tiktok"></i>
-            <i class="fa-brands fa-discord"></i>
+      <div className={styles.containerBranding}>
+        <div className={styles.wrapperContent}>
+          <div className={styles.colLeft}>
+            <div className={styles.branding}><JustaddmetaLogo /></div>
+            <p>
+              We enable your brand to <br></br>
+              make a sustainable contribution <br></br>
+              to the metaverse.
+            </p>
+            <button className={styles.button}>Enter Safe House</button>
+            <div className={styles.socialWrapper}>
+              <i class="socialIcon"><instagramOutline /></i>
+              <i class="socialIcon"><tiktokOutline /></i>
+              <i class="socialIcon"><discordOutline /></i>
+            </div>
           </div>
-        </div>
-        <div className={styles.colRight}>
-          <h3>Office Hamburg </h3>
-          <p>
-            Justaddsugar GmbH <br></br>
-            Rothenbaumchaussee 91 <br></br>
-            20148 Hamburg, Germany
-          </p>
-          <a href="tel:+4917634990063">+49 (0) 17634990063</a><br></br>
-          <a href="mailto:hello@justaddmeta.com">hello@justaddmeta.com</a>
+          <div className={styles.colRight}>
+            <h3>Office Hamburg </h3>
+            <p>
+              Justaddsugar GmbH <br></br>
+              Rothenbaumchaussee 91 <br></br>
+              20148 Hamburg, Germany
+            </p>
+            <a href="tel:+4917634990063">
+              <i class="iconPhone"><phoneOutine /></i>
+              <span class="buttonInner">+49 (0) 17634990063</span>
+            </a>
+            <a href="mailto:hello@justaddmeta.com">
+              <i class="iconPhone"><emailOutline /></i>
+              <span class="buttonInner">hello@justaddmeta.com</span>
+            </a>
           </div>
         </div>
       </div>
-      <div className={styles.container}>
-      <div className={styles.wrapper}>
-        <div className={styles.colLeft}>
-          <p>2022 © Justaddsugar - All rights reserved</p>
+      <div className={styles.containerLegal}>
+        <div className={styles.wrapperRights}>
+          <div className={styles.colLeftRights}>
+            <p>2022 © Justaddsugar - All rights reserved</p>
+          </div>
+          <div className={styles.colRightLegal}>
+            <a href="/impressum">Imprint</a>
+            <a href="/privacy-policy">Privacy Policy</a>
+          </div>
         </div>
-        <div className={styles.colRight}>
-          <a href="/impressum" class="footer-legallink">Imprint</a>
-          <a href="/privacy-policy" class="footer-legallink">Privacy Policy</a>
-        </div>
-      </div>
       </div>
 
 
     </footer>
+
 
 
 
--- components/JustaddmetaLogo.js
+++ components/JustaddmetaLogo.js
@@ -6,7 +6,7 @@ function JustaddmetaLogo(props) {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      width={"150px"}
+      width={"100%"}
       viewBox="0 0 256.34 30.87"
       {...props}
     >
