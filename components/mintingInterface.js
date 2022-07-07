29f72911d3e5e8ce46a2d8f7c0f465bfc2de51fa
Leonhard Kleinfeld <104496345+ogchectoco@users.noreply.github.com>
2022-07-07 17:29:57 +0200

    fixed className

--- components/mintingInterface.js
+++ components/mintingInterface.js
@@ -10,7 +10,7 @@ export const MintingInterface = ({ amountMinted }) => {
         <div className={styles.cardContentWrapper}>
           <div className={styles.cardImageWrapper}>
           <div className={styles.cardImage}>
-            <img class="image" src="../public/img/gläserPromo.png" alt=""></img>
+            <img className={styles.image} src="../public/img/gläserPromo.png" alt=""></img>
           </div>
           </div>
           <div className={styles.cardContent}>
