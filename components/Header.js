ebbdf9408df26c6cba4e315edf1900eb9892d3f3
Leonhard Kleinfeld <104496345+ogchectoco@users.noreply.github.com>
2022-07-07 13:46:36 +0200

    added svg to header

--- components/Header.js
+++ components/Header.js
@@ -1,6 +1,7 @@
 import React from "react";
 import styles from "../styles/header.module.css";
 import JustaddmetaLogo from "./JustaddmetaLogo";
+import { mdiWalletPlusOutline } from '@mdi/js';
 
 import {
   useAddress
@@ -21,11 +22,17 @@ export const Header = () => {
         {/* depending on an existence of a connected wallet address, it shows first or second */}
         {address ? (
           <div className={styles.navitems}>
-            <button className={styles.button}>connected</button>
+            <button className={styles.button}>
+              <Icon className={styles.icon} path={mdiWalletPlusOutline} title="Wallet" width="24px" height="24px" />
+              <span>connected</span>
+            </button>
           </div>
         ) : (
           <div className={styles.navitems}>
-            <button className={styles.button}>not connected</button>
+            <button className={styles.button}>
+              <Icon className={styles.icon} path={mdiWalletPlusOutline} title="Wallet" width="24px" height="24px" />
+              <span>not connected</span>
+            </button>
           </div>
         )}
       </div>
